cac916272e1711f8676377b3a01ad3628e2eb8b1
CBreceda <CBreceda3@MY.SCCCD.EDU>
2025-08-15 23:51:40 -0700

    Add files via upload

---
+++ script01.py
@@ -0,0 +1,8 @@
+# This is a comment, I hope I am doing this right.
+print("Hello World here is a little something I coded")
+
+num1 = 25
+num2 = 39
+
+productOfNums = num1 * num2
+print("productOfNums = " + str(productOfNums))
