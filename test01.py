5ba15b23448b7a545f0a3f3a21517c88d392bb66
CBreceda <CBreceda3@MY.SCCCD.EDU>
2025-08-16 00:03:14 -0700

    Add files via upload

---
+++ test01.py
@@ -0,0 +1,10 @@
+# Another comment here, I hope this one will work as well!
+
+print("Hello World, this feels weird typing this out in notepad!")
+
+num1 = 10
+num2 = 5
+
+sumOfNums = num1 + num2
+
+print("The sum of " + str(num1) + " and " + str(num2) + " is: " + str( sumOfNums))
